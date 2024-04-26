8feab6dc2db226ded6812a06d74a4c288ade307b
Rafaelbadas <rafacce233@gmail.com>
2024-04-26 18:19:02 -0300

    Login funcional e pagina test, para teste ;)

--- view/login.php
+++ view/login.php
@@ -1,3 +1,8 @@
+<?php
+    ob_start();
+  $banco = new PDO('mysql:host=localhost;dbname=rhdaz', 'root','1234');
+  print "Conexão Efetuada com sucesso!";
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -19,63 +24,79 @@
 </head>
 
 <body>
-    <section class="h-100 gradient-form" style="background-color: #eee;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
+    <form method="post" action="#">
+        <section class="h-100 gradient-form" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
 
-                                    <div class="text-center">
-                                        <img src="img/logo.svg" style="width: 75px;" alt="logo">
-                                        <h4 class="mt-1 mb-5 pb-1">NexusSync</h4>
-                                    </div>
-
-                                    <form>
-                                        <p>Faça login com sua conta</p>
-
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <input type="email" id="form2Example11" class="form-control"
-                                                placeholder="E-mail" />
+                                        <div class="text-center">
+                                            <img src="img/logo.svg" style="width: 75px;" alt="logo">
+                                            <h4 class="mt-1 mb-5 pb-1">NexusSync</h4>
                                         </div>
 
-                                        <div data-mdb-input-init class="form-outline mb-4">
-                                            <input type="password" id="form2Example22" class="form-control" placeholder="Senha" />
-                                    
-                                        </div>
+                                        <form>
+                                            <p>Faça login com sua conta</p>
 
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <button data-mdb-button-init data-mdb-ripple-init
+                                            <div data-mdb-input-init class="form-outline mb-4">
+                                                <input type="text" name="email" 
+                                                id="email" class="form-control" placeholder="E-mail" />
+                                            </div>
+
+                                            <div data-mdb-input-init class="form-outline mb-4">
+                                                <input type="password" name="senha" 
+                                                id="senha" class="form-control" placeholder="Senha" />
+                                        
+                                            </div>
+
+                                            <div class="text-center pt-1 mb-5 pb-1">
+                                                <input data-mdb-button-init data-mdb-ripple-init
                                                 class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                type="button">Login</button>
+                                                type="submit" name="login" value="Login">
                                                 <br>
-                                            <a class="text-muted" href="#!">Esqueceu a senha?</a>
-                                        </div>
+                                                <a class="text-muted" href="#!">Esqueceu a senha?</a>
+                                            </div>
 
-                                    </form>
+                                        </form>
 
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                    <h4 class="mb-4">MISSÃO: </h4>
-                                    <p class="small mb-0">Nossa missão é oferecer soluções de software de alta qualidade
-                                        e alto desempenho, que simplifiquem e otimizem processos de gestão, promovendo a
-                                        eficiência operacional, a transparência e a satisfação dos clientes.</p>
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <h4 class="mb-4">MISSÃO: </h4>
+                                        <p class="small mb-0">Nossa missão é oferecer soluções de software de alta qualidade
+                                            e alto desempenho, que simplifiquem e otimizem processos de gestão, promovendo a
+                                            eficiência operacional, a transparência e a satisfação dos clientes.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </form>
 
-
-
-
+    <?php
+        $chamada = $banco->query("SELECT adm_email, adm_senha FROM adm_administradorRH;");
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        if(isset($_POST['login'])){
+            
+            while($linha = $chamada->fetch(PDO::FETCH_ASSOC)) {
+                echo $email . " " . $senha;
+                echo "Email: {$linha['adm_email']} - Senha: {$linha['adm_senha']} <br />";
+                if($email == $linha['adm_email'] && $senha == $linha['adm_senha']){
+                    header("Location: index.php");
+                    exit;
+                }
+            }
+        }
+    ?>
 
 </body>
 
