f5bd5c4a7c4363426b80fe56138e467a21710c3c
Rafaelbadas <59000602+Rafaelbadas@users.noreply.github.com>
2024-05-15 18:23:08 -0300

    controller adjusts

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -6,58 +6,69 @@
     ob_start();
     
     class LoginController extends LoginModel{
+        public function __construct(){
+            
+        }
         public function BDConnection(){
             return new PDO('mysql:host=localhost;dbname=NexusRH', 'root','1234');
         }
         public function BDLogin($email, $senha, $login){
             $banco = self::BDConnection();
-            $chamada = $banco->query("SELECT usu_email, usu_senha FROM usu_usuarios;");
+            $chamada = $banco->query("SELECT usu_id, usu_email, usu_senha FROM usu_usuarios;");
             $modelLogin = self::VerificarLogin($chamada, $email, $senha, $login);
-            $LModel = new LoginModel;
             if($modelLogin == true){
-                $LModel->setUsuario($email, $senha);
+                parent::setUsuario($email, $senha);
                 $tipo = self::VerificarTipo($email);
-                $LModel->setTipo($tipo);
-                echo $LModel->getTipo();
+                parent::setTipo($tipo);
                 if($tipo == "colaborador"){
                     header("Location: IndexColaborador.php");
                 }else if($tipo == "administrador"){
                     header("Location: IndexAdministrador.php");
                 }
                 return true;
-                //exit;
-                // $_SESSION['login'] = $email;
-                // session_unset('erro');
             }else{
-                $LModel->getErro();
-                return $LModel->erro;
-                // $_SESSION['erro'] = "Falha no Login, usuário não identificado"; 
-                // return "Falha no Login, usuário não identificado";
-                // $_SESSION['login'] = $email;
-                // $_SESSION['erro'] = "wdadaw";
-                // session_unset('login');
+                return parent::getErro();;
             }
         }
         public function VerificarLogin($chamada, $email, $senha, $login){
             if(isset($login)){
                 while($linha = $chamada->fetch(PDO::FETCH_ASSOC)) {
-                    //echo $email . " " . $senha;
-                    //echo "<br>Email: {$linha['adm_email']} - Senha: {$linha['adm_senha']} <br />";
-                    if($email == $linha['usu_email'] && $senha == $linha['usu_senha']){
-                        return true;
-                    }else{
-                        //return false;
+                    if($email == $linha['usu_email']){
+                        return self::VerificarCriptografia($senha, hash('sha512', $senha), $linha['usu_senha'], $linha['usu_id']);
                     }
                 }
             }
         }
+        public function VerificarCriptografia($senha, $senCriptografada, $senhaDB, $idDB){
+            if(isset($senha)){
+                if($senha == $senhaDB){
+                    self::CriptografarSenha($senhaDB, $idDB);
+                    parent::setID($idDB);
+                    return true;
+                }
+                if($senCriptografada == $senhaDB){
+                    parent::setID($idDB);
+                    return true;
+                }
+                return false;
+            }
+        }
+        public function CriptografarSenha($senha, $idDB){
+            $senhaCriptografada = hash('sha512', $senha);
+            $stmt = self::BDConnection()->prepare('UPDATE usu_usuarios SET usu_senha = :criptografada 
+            WHERE usu_id = :id');
+            $stmt->bindParam(':criptografada', $senhaCriptografada);
+            $stmt->bindParam(':id', $idDB);
+            return $stmt->execute();
+        }
         public function VerificarTipo($email){
             $banco = self::BDConnection();
-            //$chamada = $banco->query("SELECT adm_email, adm_tipo FROM adm_administradorRH WHERE '$email' = adm_email;");
             $chamada = $banco->
-            query("SELECT usu_email, 'administrador' as tipo from usu_usuarios u inner join adm_administradorRH a on u.usu_id = a.usu_id where '$email' = usu_email
+            query("SELECT usu_email, 'administrador' as tipo from usu_usuarios u 
+            inner join adm_administradorRH a on u.usu_id = a.usu_id where '$email' = usu_email
             union
-            select usu_email, 'colaborador' as tipo from usu_usuarios u inner join col_colaborador c on u.usu_id = c.usu_id where '$email' = usu_email;");
+            select usu_email, 'colaborador' as tipo from usu_usuarios u 
+            inner join col_colaborador c on u.usu_id = c.usu_id where '$email' = usu_email;");
             $linha = $chamada->fetchAll(PDO::FETCH_ASSOC);
             $linhaLength = count($linha);
             for($i=0;$i<= $linhaLength;$i++){
@@ -66,9 +77,13 @@
                 }
             }
         }
+        
         static function getUsuario(){
             $userInfo = new LoginModel;
             return $userInfo::getUsuario();
+        }
+        static function getID(){
+            return parent::getID();
         }
         static function destroy_sessoes(){
             $sessoes = new LoginModel;
@@ -78,12 +93,5 @@
             $LMode = new LoginModel;
             return $LMode->getTipo();
         }
-        /*public function getErro(){
-            return $this->erro;
-        }
-        public function setErro(){
-            $this->erro = $_SESSION['erro'];
-        }
-        Passar para o model */
     }
 ?>
